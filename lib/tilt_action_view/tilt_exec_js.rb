5e749a1c324f99d78b45498cbe3c887f22a1fa35
Alex Riedler <alex.riedler@500px.com>
2013-05-29 15:42:32 -0400

    blargh

--- lib/tilt_action_view/tilt_exec_js.rb
+++ lib/tilt_action_view/tilt_exec_js.rb
@@ -8,11 +8,11 @@ module TiltActionView
     end
 
     def evaluate(scope, locals, &block)
-      ExecJS.compile("#{@tilt_template_class.evaluate(scope,locals, &block)}\nthis.JST[#{file}]()")
+      ExecJS.compile("#{@tilt_template_class.evaluate(scope, locals, &block)}\nthis.JST[#{file}]()")
     end
 
     def self.handler_for(tilt_template)
-      handler = Class.new(ActionViewHandler)
+      handler = Class.new(TiltExecJS)
       handler.instance_variable_set :@tilt_template_class, tilt_template
       handler
     end
